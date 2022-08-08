336049c795a0bdb8e58bad6f9f50aad46117faf3
joko19 <jokoslamet459@gmail.com>
2022-08-09 00:32:47 +0700

    key index

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
     <div className="p-4">
       <div className="flex flex-wrap">
         {categories.map((value, index) => (
-          <div className="grow w-64">
+          <div className="grow w-64" key={index}>
             <div className="bg-black rounded-lg m-1 drop-shadow-xl cursor-pointer relative grow  hover:font-bold hover:bg-gray-900">
               <img src={`/images/${value[0]}.png`} className="w-full h-48 object-cover opacity-40 rounded-lg drop-shadow-xl" />
               <h1 className="text-2xl absolute bottom-2 left-2 text-white"> {value[0]}</h1>
